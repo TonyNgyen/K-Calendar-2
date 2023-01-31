3c5e8f7f6b20a907251d2ad39a406c98d55ccd2a
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-01-30 19:15:04 -0800

    Update ReleaseCard.js
    Updated how release cards look. I think they look more streamlined and less cluttered now

--- Components/ReleaseCard.js
+++ Components/ReleaseCard.js
@@ -6,19 +6,34 @@ export default function ReleaseCard(props) {
   const day = props.date.split("/")[1];
   
   return (
-    <div className="text-center rounded-2xl mx-auto overflow-hidden max-w-sm bg-white shadow-lg shadow-gray-300 font-poppins pb-5">
-      <div className="m-4">
-        <Link className='group text-black transition-all duration-300 ease-in-out' href={{
-          pathname: "artists/" + [props.artist]
-        }}>
-          <span className='text-2xl bg-left-bottom bg-gradient-to-r from-lightmodeRed to-lightmodeRed bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out text-lightmodeRed'>
-            {props.artist}
-          </span>
-        </Link>
-      </div>
+    <div className="rounded-2xl mx-auto overflow-hidden max-w-sm bg-white shadow-lg shadow-gray-300 font-poppins pb-5">
       <Image src={props.image} alt="Image of Artist" width={640} height={640} className="aspect-square object-cover object-center" loading="lazy"/>
-      <h3 className="mt-3 mb-2 text-xl">{props.name}</h3>
-      <h4 className="my-3 bg-lightmodeRed px-2 inline rounded-lg text-white">{day + (day>10 && day<20 ? 'th' : {1:'st', 2:'nd', 3:'rd'}[day % 10] || 'th')}</h4>
+      <div className="flex items-center pl-5 pt-3">
+        <Link className='group text-black transition-all duration-300 ease-in-out mr-3' href={{
+            pathname: "artists/" + [props.artist]
+          }}>
+            <span className='text-3xl bg-left-bottom bg-gradient-to-r from-lightmodeRed to-lightmodeRed bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out text-lightmodeRed'>
+              {props.artist}
+            </span>
+        </Link>
+        <h4 className="bg-lightmodeRed px-2 inline rounded-lg text-white text-md">{day + (day>10 && day<20 ? 'th' : {1:'st', 2:'nd', 3:'rd'}[day % 10] || 'th')}</h4>
+      </div>
+      <h3 className="pl-5 mb-2 mt-2 text-xl">{props.name}</h3>
     </div>
   )
 }
+
+{/* <div className="text-center rounded-2xl mx-auto overflow-hidden max-w-sm bg-white shadow-lg shadow-gray-300 font-poppins pb-5">
+<div className="m-4">
+  <Link className='group text-black transition-all duration-300 ease-in-out' href={{
+    pathname: "artists/" + [props.artist]
+  }}>
+    <span className='text-2xl bg-left-bottom bg-gradient-to-r from-lightmodeRed to-lightmodeRed bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out text-lightmodeRed'>
+      {props.artist}
+    </span>
+  </Link>
+</div>
+<Image src={props.image} alt="Image of Artist" width={640} height={640} className="aspect-square object-cover object-center" loading="lazy"/>
+<h3 className="mt-3 mb-2 text-xl">{props.name}</h3>
+<h4 className="my-3 bg-lightmodeRed px-2 inline rounded-lg text-white">{day + (day>10 && day<20 ? 'th' : {1:'st', 2:'nd', 3:'rd'}[day % 10] || 'th')}</h4>
+</div> */}
