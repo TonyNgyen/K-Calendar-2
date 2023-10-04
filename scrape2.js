b6ff2a9bf78fdceff09cb0731b3b6b969dffffc3
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-10-04 12:43:48 -0700

    Developing scraping reddit with puppeteer

--- scrape2.js
+++ scrape2.js
@@ -102,50 +102,45 @@ class Releases {
         });
         table;
     }
-    getQuotes = async () => {
+
+    async getQuotes() {
         // Start a Puppeteer session with:
         // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
         // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
         const browser = await puppeteer.launch({
-          headless: false,
-          defaultViewport: null,
+            headless: false,
+            defaultViewport: null,
         });
-      
+
         // Open a new page
         const page = await browser.newPage();
-      
+
         // On this new page:
-        // - open the "http://quotes.toscrape.com/" website
+        // - opens to reddit
         // - wait until the dom content is loaded (HTML is ready)
-        await page.goto("https://kpopofficial.com/kpop-comeback-schedule-october-2023/", {
-          waitUntil: "domcontentloaded",
+        await page.goto("https://www.reddit.com/r/kpop/wiki/upcoming-releases/2023/october/", {
+            waitUntil: "domcontentloaded",
         });
-      
+
         // Get page data
-        const quotes = await page.evaluate(() => {
-          // Fetch the first element with class "quote"
-          // Get the displayed text and returns it
-          const quoteList = document.querySelectorAll("tr");
-          return quoteList;
-      
-          // Convert the quoteList to an iterable array
-          // For each quote fetch the text and author
-          return Array.from(quoteList).map((quote) => {
-            // Fetch the sub-elements from the previously fetched quote element
-            // Get the displayed text and return it (`.innerText`)
-            const text = quote.querySelector(".text").innerText;
-            const author = quote.querySelector(".author").innerText;
-      
-            return { text, author };
-          });
+        const tableData = await page.evaluate(() => {
+            // Fetch the first element with class "quote"
+            // Get the displayed text and returns it
+            const rows = document.querySelectorAll('table tr');
+                return Array.from(rows, row => {
+                  const columns = row.querySelectorAll('td');
+                  return Array.from(columns, column => column.innerText);
+            });
+            // const tds = Array.from(document.querySelectorAll('table tr td'))
+            // return tds.map(td => td.innerText)
         });
-      
-        // Display the quotes
-        console.log(quotes);
-      
+
+        // Display the tableData
+        console.log(tableData[1]);
+
         // Close the browser
         await browser.close();
-      };
+    };
 }
 
 let releases = new Releases();
