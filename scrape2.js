9ed685c3b604e852da8facb36df4d52428406e97
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-10-04 10:51:45 -0700

    Switching from axios to puppeteer for scraping

--- scrape2.js
+++ scrape2.js
@@ -1,77 +1,90 @@
 const axios = require("axios");
+const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 const mongoose = require("mongoose");
 
 class Releases {
-  // Connect to mongoDB
-  async connect() {
-    try {
-      let connection = await mongoose.connect(
-        "mongodb+srv://tony:mHRUNHNCWtVKLRbC@cluster0.kljmqtn.mongodb.net/KCalendar?retryWrites=true&w=majority"
-      );
-      mongoose.set("strictQuery", false);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  // Disconnect from mongoDB
-  async disconnect() {
-    mongoose.disconnect();
-    console.log("THIS WORKS2");
-  }
-
-  constructor() {
-    this.connect();
-    this.upcomingReleases;
-    this.pastReleases;
-    this.releaseSchema = new mongoose.Schema({
-        "date": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "time": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "artist": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "artist image": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "name": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "album type": {
-            type: String,
-            required: [true, "N/A"]
-        },
-        "title track": {
-            type: String,
-            required: [true, "N/A"]
+    // Connect to mongoDB
+    async connect() {
+        try {
+            let connection = await mongoose.connect(
+                "mongodb+srv://tony:mHRUNHNCWtVKLRbC@cluster0.kljmqtn.mongodb.net/KCalendar?retryWrites=true&w=majority"
+            );
+            mongoose.set("strictQuery", false);
+            console.log("THIS WORKS")
+        } catch (err) {
+            console.log(err);
         }
-    })
-    this.groupSchema = new mongoose.Schema({
-        "name": {
-            type: String,
-            required: [true, "No name specified"]
-        },
-        "followers": {
-            type: Number,
-            min: 0,
-        },
-        "artist image": {
-            type: String,
-            required: [true, "https://i.scdn.co/image/ab6761610000e5ebb1a15fd3e7c1b375dea2637a"]
-        },
-        "releases": {},
-        "related artists":{},
-    })
-}
+    }
+
+    // Disconnect from mongoDB
+    async disconnect() {
+        mongoose.disconnect();
+    }
+
+    constructor() {
+        this.connect();
+        this.upcomingReleases;
+        this.pastReleases;
+        this.releaseSchema = new mongoose.Schema({
+            "date": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "time": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "artist": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "artist image": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "name": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "album type": {
+                type: String,
+                required: [true, "N/A"]
+            },
+            "title track": {
+                type: String,
+                required: [true, "N/A"]
+            }
+        })
+        this.groupSchema = new mongoose.Schema({
+            "name": {
+                type: String,
+                required: [true, "No name specified"]
+            },
+            "followers": {
+                type: Number,
+                min: 0,
+            },
+            "artist image": {
+                type: String,
+                required: [true, "https://i.scdn.co/image/ab6761610000e5ebb1a15fd3e7c1b375dea2637a"]
+            },
+            "releases": {},
+            "related artists": {},
+        })
+    }
+
+    async scrape() {
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+        });
+        const page = await browser.newPage();
+        await page.goto("http://quotes.toscrape.com/", {
+            waitUntil: "domcontentloaded",
+        });
+    }
 }
 
 let releases = new Releases();
+releases.scrape();
