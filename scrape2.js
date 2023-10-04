0e6e75d97fafc450130565dddf905d6d6263cf76
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-10-04 11:36:17 -0700

    Getting puppeteer to work

--- scrape2.js
+++ scrape2.js
@@ -11,7 +11,6 @@ class Releases {
                 "mongodb+srv://tony:mHRUNHNCWtVKLRbC@cluster0.kljmqtn.mongodb.net/KCalendar?retryWrites=true&w=majority"
             );
             mongoose.set("strictQuery", false);
-            console.log("THIS WORKS")
         } catch (err) {
             console.log(err);
         }
@@ -80,11 +79,74 @@ class Releases {
             defaultViewport: null,
         });
         const page = await browser.newPage();
-        await page.goto("http://quotes.toscrape.com/", {
+        await page.goto("https://www.reddit.com/r/kpop/wiki/upcoming-releases/2023/october/", {
             waitUntil: "domcontentloaded",
         });
+        const table = await page.evaluate(() => {
+            // Fetch the first element with class "quote"
+            // Get the displayed text and returns it
+            const tableData = document.getElementsByTagName("table");
+            console.log(tableData);
+            return tableData;
+
+            // // Convert the quoteList to an iterable array
+            // // For each quote fetch the text and author
+            // return Array.from(quoteList).map((quote) => {
+            //   // Fetch the sub-elements from the previously fetched quote element
+            //   // Get the displayed text and return it (`.innerText`)
+            //   const text = quote.querySelector(".text").innerText;
+            //   const author = quote.querySelector(".author").innerText;
+
+            //   return { text, author };
+            // });
+        });
+        table;
     }
+    getQuotes = async () => {
+        // Start a Puppeteer session with:
+        // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+        // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
+        const browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null,
+        });
+      
+        // Open a new page
+        const page = await browser.newPage();
+      
+        // On this new page:
+        // - open the "http://quotes.toscrape.com/" website
+        // - wait until the dom content is loaded (HTML is ready)
+        await page.goto("https://kpopofficial.com/kpop-comeback-schedule-october-2023/", {
+          waitUntil: "domcontentloaded",
+        });
+      
+        // Get page data
+        const quotes = await page.evaluate(() => {
+          // Fetch the first element with class "quote"
+          // Get the displayed text and returns it
+          const quoteList = document.querySelectorAll("tr");
+          return quoteList;
+      
+          // Convert the quoteList to an iterable array
+          // For each quote fetch the text and author
+          return Array.from(quoteList).map((quote) => {
+            // Fetch the sub-elements from the previously fetched quote element
+            // Get the displayed text and return it (`.innerText`)
+            const text = quote.querySelector(".text").innerText;
+            const author = quote.querySelector(".author").innerText;
+      
+            return { text, author };
+          });
+        });
+      
+        // Display the quotes
+        console.log(quotes);
+      
+        // Close the browser
+        await browser.close();
+      };
 }
 
 let releases = new Releases();
-releases.scrape();
+releases.getQuotes();
